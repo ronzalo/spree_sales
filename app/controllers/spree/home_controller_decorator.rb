2f479de518ed1d478ea8373bf01ed28e8a4c3162
Gonzalo <gmoreno@acid.cl>
2014-03-10 15:58:01 -0400

    update version

--- app/controllers/spree/home_controller_decorator.rb
+++
@@ -1,8 +0,0 @@
-module Spree
-  HomeController.class_eval do
-    def sale
-      # TODO Refactor to include sales in searcher builder
-      @products = Product.joins(:master => :sale_prices).uniq
-    end
-  end
-end
