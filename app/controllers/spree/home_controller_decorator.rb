5f97bde5caec0460c297ff26606909cc8346b32a
mespina <mespina@acid.cl>
2014-03-10 22:48:07 -0400

    Sales by taxon

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -3,10 +3,10 @@ module Spree
     def sale
       # TODO Refactor to include sales in searcher builder
       if params[:id]
-        @taxon = Spree::Taxon.by_store(current_store).find_by_permalink!(params[:id])
-        @products = Spree::Product.by_store(current_store.id).in_sale.in_taxon(@taxon)
+        @taxon = Spree::Taxon.find_by_permalink!(params[:id])
+        @products = Spree::Product.in_sale.in_taxon(@taxon)
       else
-        @products = Spree::Product.by_store(current_store.id).in_sale
+        @products = Spree::Product.in_sale
       end
     end
   end
