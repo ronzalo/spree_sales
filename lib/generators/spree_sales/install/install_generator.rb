39e5a7afbd023810a56e4fbef3c7249692b76c0b
Gonzalo Moreno <gmoreno@acid.cl>
2015-01-14 12:15:41 -0300

    remove unused assets to make compatible with 2-2 branch

--- lib/generators/spree_sales/install/install_generator.rb
+++ lib/generators/spree_sales/install/install_generator.rb
@@ -4,16 +4,6 @@ module SpreeSales
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_sales\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_sales\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_sales\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_sales\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_sales'
       end
