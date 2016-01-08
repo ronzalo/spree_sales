24ce5b7ca2d407ec734b31b18c4e2c970c2e0c43
Gonzalo Moreno <gmoreno@acid.cl>
2016-01-08 10:59:21 -0300

    Update 20150718115800_rename_spree_sales_calculator.rb

--- db/migrate/20150718115800_rename_spree_sales_calculator.rb
+++ db/migrate/20150718115800_rename_spree_sales_calculator.rb
@@ -1,9 +1,9 @@
 class RenameSpreeSalesCalculator < ActiveRecord::Migration
   def up
-    execute "UPDATE `spree_calculators` SET type = 'Spree::Calculator::AmountSalePriceCalculator' WHERE type = 'Spree::Calculator::DollarAmountSalePriceCalculator'"
+    execute "UPDATE spree_calculators SET type = 'Spree::Calculator::AmountSalePriceCalculator' WHERE type = 'Spree::Calculator::DollarAmountSalePriceCalculator'"
   end
 
   def down
-    execute "UPDATE `spree_calculators` SET type = 'Spree::Calculator::DollarAmountSalePriceCalculator' WHERE type = 'Spree::Calculator::AmountSalePriceCalculator'"
+    execute "UPDATE spree_calculators SET type = 'Spree::Calculator::DollarAmountSalePriceCalculator' WHERE type = 'Spree::Calculator::AmountSalePriceCalculator'"
   end
 end
